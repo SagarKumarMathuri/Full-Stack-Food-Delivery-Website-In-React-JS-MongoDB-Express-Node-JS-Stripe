a1bc7a43cd63844958d6d7b591976baa625e5324
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-19 12:06:06 +0530

    Did some changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the freshest ingredients, ensuring a delightful dining
+          experience delivered right to your doorstep.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
