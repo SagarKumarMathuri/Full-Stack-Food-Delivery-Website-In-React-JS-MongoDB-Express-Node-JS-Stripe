e16ac62c4ca7f0f57b2701c6ac1a78a73a565d6e
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-27 12:00:44 +0530

    Did Some changes

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-  return <div></div>;
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
