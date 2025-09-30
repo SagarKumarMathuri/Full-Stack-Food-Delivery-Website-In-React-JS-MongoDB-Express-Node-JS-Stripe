bbf743a6fa8604a55844bbff491c40689ebe6014
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-30 09:38:06 +0530

    Did Some changes

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./PlaceOrder.css";
 
 const PlaceOrder = () => {
-  return <div></div>;
+  return (
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" PlaceOrder="First name " />
+          <input type="text" PlaceOrder="Last name" />
+        </div>
+        <input type="text" />
+        <input type="text" />
+      </div>
+      <div className="place-order-right"></div>
+    </form>
+  );
 };
 
 export default PlaceOrder;
